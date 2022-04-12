4942a387876febedd3670944c06d470bc7639ad8
Ana Zitzow <anasempai89@gmail.com>
2022-04-11 19:10:44 -0700

    commit tree 3415ad76e3e82965517f78345637ac898a3e4593

---
+++ index.js
@@ -0,0 +1,33 @@
+let add = function (a, b) {
+  return a + b;
+};
+
+let subtract = function (a, b) {
+  return a - b;
+};
+
+let multiply = function (a, b) {
+  return a * b;
+};
+
+let divide = function (a, b) {
+  return a / b;
+};
+
+let increment = function (n) {
+  n++;
+  return n;
+};
+
+let decrement = function (n) {
+  n--;
+  return n;
+};
+
+let makeInt = function (n) {
+  return parseInt(n, 10);
+};
+
+let preserveDecimal = function (n) {
+  return parseFloat(n);
+};
